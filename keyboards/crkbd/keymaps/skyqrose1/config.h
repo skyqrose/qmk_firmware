1c46edfd891053d294eb4634d74a5190d98d8518
Sky Rose <zgnomdnv@sky.skyqrose.com>
2021-08-22 19:49:22 -0400

    toggle layers with just one keypress

--- keyboards/crkbd/keymaps/skyqrose1/config.h
+++ keyboards/crkbd/keymaps/skyqrose1/config.h
@@ -31,6 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
+#define TAPPING_TOGGLE 1
+#define PERMISSIVE_HOLD
 
 #define OLED_FONT_H "keyboards/crkbd/keymaps/skyqrose1/glcdfont.c"
 
